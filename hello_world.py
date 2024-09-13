24a39001881ef4332acdbf7467d0191dac2e8178
1s20t <kyw0708@kumoh.ac.kr>
2024-09-13 10:27:58 +0900

    simple changes

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,9 @@
 def print_hello():
     print("Hello World!!")
+    print("Hello World!!")
+    print("Hello World!!")
+    print("Hello World!!")
+    print("Hello World!!")
 
 if __name__ == '__main__':
     print_hello()
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import max_profit as mp
 import prime_number as pn
+
+print("blabla")
+print("I use arch btw")
 
 if __name__ == '__main__':
     pn.is_prime_number(1)
     pn.is_prime_number(13)
-    pn.is_prime_number(24)
-    pn.is_prime_number(31)
-    pn.is_prime_number(45)
     pn.is_prime_number(97)
